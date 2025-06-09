b68a5b1597a532ea269b7f134b25021f73bbb08e
Daniel Savage <dansvg@gmail.com>
2025-06-09 11:10:32 +0900

    Add phantom camera

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -2,6 +2,8 @@ class_name Gameplay extends Node3D
 
 @onready var player_marble: PlayerControl = $PlayerMarble
 
+@onready var gameplay_camera: PhantomCamera3D = $GameplayCamera
+
 func get_starting_positions() -> Array[StartPoint]:
 	var result: Array[StartPoint] = []
 	
@@ -16,6 +18,10 @@ func set_player_to_starting_position(player: PlayerControl, index: int = 0) -> v
 	var start_point_to_use := starting_positions[index % starting_positions.size()]
 	player.global_position = start_point_to_use.global_position
 
-# Called when the node enters the scene tree for the first time.
+func setup_camera() -> void:
+	gameplay_camera.set_follow_target(player_marble)
+	gameplay_camera.set_look_at_target(player_marble)
+
 func _ready() -> void:
+	setup_camera()
 	set_player_to_starting_position(player_marble, 1)
