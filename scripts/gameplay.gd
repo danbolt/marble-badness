b34fd3196526dd6dfddaa4afd73428e4b10c930a
Daniel Savage <dansvg@gmail.com>
2025-06-29 15:12:13 +0900

    Clean up GDScript errors and warnings

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -4,7 +4,6 @@ class_name Gameplay extends Node3D
 
 @onready var gameplay_camera: PhantomCamera3D = $GameplayCamera
 
-@onready var post_process_component: Node3D = $PostProcessMesh
 # This is assigned on level load
 var level: Node = null
 
@@ -26,8 +25,6 @@ func set_player_to_starting_position(player: PlayerControl, index: int = 0) -> v
 func setup_camera() -> void:
 	gameplay_camera.set_follow_target(player_marble)
 	gameplay_camera.set_look_at_target(player_marble)
-	#gameplay_camera.add_child(post_process_component)
-	#print(post_process_component)
 	
 
 func setup_level(level_to_setup: PackedScene) -> void:
